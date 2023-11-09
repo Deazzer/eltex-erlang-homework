9da1095760c356a661070f76b8be55b8e50a280e
unknown <kuanishevv@gmail.com>
2023-11-09 18:47:37 +0700

    fixedwork6

---
+++ H7/keylist.erl
@@ -0,0 +1,53 @@
+-module(keylist).
+
+-export([loop/1, start_monitor/1, start_link/1]).
+
+-record(state, {
+    
+    list = [], 
+    counter = 0
+}).
+
+start_monitor(Name) ->
+    Pid = spawn(keylist, loop, [#state{}]),
+        register(Name, self()),
+            MonitorRef = erlang:monitor(process, Pid),
+		        {ok, Pid, MonitorRef}.
+
+start_link(Name) ->
+	Pid = spawn(keylist, loop, [#state{}]),
+        link(Pid),
+            register(Name, Pid),    
+                {ok, Pid}.
+loop(#state{list = List, counter = Counter} = State) ->
+    receive
+        
+        {From, add, Key, Value, Comment} ->
+            NewState = State#state{list = [{Key, Value, Comment} | List], counter = Counter + 1},
+                From ! {ok, NewState#state.counter},
+                    loop(NewState);
+
+        {From, is_member, Key} ->
+            Result = lists:keymember(Key, List, 1),
+                From ! Result,
+                    loop(State);
+
+        {From, take, Key} ->
+            {Value, Comment, Rest} = lists:keytake(Key, 1, List),
+                NewState = State#state{list = Rest, counter = Counter + 1},
+                    From ! {ok, {Value, Comment}, NewState#state.counter},
+                        loop(NewState);
+
+        {From, find, Key} ->
+            Result = lists:keyfind(Key, List, 1),
+                From ! Result,
+                    loop(State);
+
+        {From, delete, Key} ->
+            NewList = lists:keydelete(Key, 1, State#state.list),
+                NewState = State#state{list = NewList, counter = Counter + 1},
+                    From ! {ok, NewState#state.counter},
+                        loop(NewState)
+
+    end.
+
